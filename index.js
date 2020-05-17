d964a2338fd727a3b2c538c027cefbb33d1e96ff
Akshat Khanna <khannakshat7@gmail.com>
2020-05-17 15:39:37 +0530

    Data from database for mail

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const app = express();
 global.__basedir = __dirname;
 
 const date = new Date();
-const datetime = date.getDate()+" "+date.getMonth()+" "+date.getFullYear()+"-"+date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
-
+const datetime = date.getDate()+" "+date.getMonth()+" "+date.getFullYear();
+// +"-"+date.getHours()+":"+date.getMinutes()+":"+date.getSeconds()
 // -> Multer Upload Storage
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -26,12 +26,8 @@ const upload = multer({storage: storage});
 // -> Express Upload RestAPIs
 app.post('/api/uploadfile', upload.single("uploadfile"), (req, res) =>{
   importExcelData2MySQL(__basedir + '/uploads/' + req.file.filename);
-  res.json({
-        'msg': 'File uploaded/import successfully!', 'file': req.file, 'Date Uploaded':datetime
-      });
-});
- 
-// -> Import Excel Data to MySQL database
+
+  // -> Import Excel Data to MySQL database
 function importExcelData2MySQL(filePath){
   // File path.
   readXlsxFile(filePath).then((rows) => {
@@ -41,18 +37,26 @@ function importExcelData2MySQL(filePath){
     // Remove Header ROW
     rows.shift();
 	// MySQL data insert using previous connection
-	let queryinit= 'CREATE TABLE test1 (ZONE varchar(255),LOCATION_CODE varchar(255),TANK_TRUCK_NUMBER varchar(255),TRANSPORTER_CODE varchar(255),TRANSPORTER_NAME varchar(255),TOTAL_NO_OF_TRIPS varchar(255),TOTAL_TRIPS_WITH_VOILATION varchar(255),NO_OF_SPEED_VOILATIONS varchar(255),NO_OF_STOPPAGE_VOILATIONS varchar(255),NO_OF_ROUTE_VOILATIONS varchar(255),FROM_DATE varchar(255),TO_DATE varchar(255),EMAIL varchar(255));';
+	  let queryinit= 'CREATE TABLE `'+ datetime +'` (ZONE varchar(255),LOCATION_CODE varchar(255),TANK_TRUCK_NUMBER varchar(255),TRANSPORTER_CODE varchar(255),TRANSPORTER_NAME varchar(255),TOTAL_NO_OF_TRIPS varchar(255),TOTAL_TRIPS_WITH_VOILATION varchar(255),NO_OF_SPEED_VOILATIONS varchar(255),NO_OF_STOPPAGE_VOILATIONS varchar(255),NO_OF_ROUTE_VOILATIONS varchar(255),FROM_DATE varchar(255),TO_DATE varchar(255));';
         connection.query(queryinit,(error, response) => {
 			if(error){
         console.log(error);}
         });
-    let query = 'INSERT INTO test1 (`ZONE`, `LOCATION_CODE`, `TANK_TRUCK_NUMBER`, `TRANSPORTER_CODE`, `TRANSPORTER_NAME`, `TOTAL_NO_OF_TRIPS`, `TOTAL_TRIPS_WITH_VOILATION`, `NO_OF_SPEED_VOILATIONS`, `NO_OF_STOPPAGE_VOILATIONS`, `NO_OF_ROUTE_VOILATIONS`, `FROM_DATE`, `TO_DATE`, `EMAIL`) VALUES ?';
+        let query = 'INSERT INTO `'+ datetime +'` (`ZONE`, `LOCATION_CODE`, `TANK_TRUCK_NUMBER`, `TRANSPORTER_CODE`, `TRANSPORTER_NAME`, `TOTAL_NO_OF_TRIPS`, `TOTAL_TRIPS_WITH_VOILATION`, `NO_OF_SPEED_VOILATIONS`, `NO_OF_STOPPAGE_VOILATIONS`, `NO_OF_ROUTE_VOILATIONS`, `FROM_DATE`, `TO_DATE`) VALUES ?';
         connection.query(query, [rows], (error, response) => {
 			if(error){
         console.log(error);}
         });
   })
 }
+
+    
+  res.json({
+        'msg': 'File uploaded/import successfully!', 'file': req.file, 'Date Uploaded':datetime
+      });
+});
+ 
+
  
 /* SQL BEGINS */
 
@@ -103,7 +107,7 @@ app.get('/upload', (req, res) => {
 
 //total violations
 app.get('/totalviolations', (req, res) => {
-  let sql = 'SELECT emaildetails.EMAIL FROM test1 INNER JOIN emaildetails ON emaildetails.TRANSPORTER_CODE = Test1.TRANSPORTER_CODE WHERE test1.TOTAL_TRIPS_WITH_VOILATION>2;';
+  let sql = 'SELECT emaildetails.EMAIL FROM `'+ datetime +'` INNER JOIN emaildetails ON emaildetails.TRANSPORTER_CODE = `'+ datetime +'`.TRANSPORTER_CODE WHERE `'+ datetime +'`.TOTAL_TRIPS_WITH_VOILATION>2;';
   let query = connection.query(sql, (err,result)=>{
       if(err){
           throw err;
@@ -117,7 +121,7 @@ app.get('/totalviolations', (req, res) => {
 
 //speed violations
 app.get('/speedviolations', (req, res) => {
-  let sql = 'SELECT emaildetails.EMAIL FROM test1 INNER JOIN emaildetails ON emaildetails.TRANSPORTER_CODE = Test1.TRANSPORTER_CODE WHERE test1.NO_OF_SPEED_VOILATIONS>0;';
+  let sql = 'SELECT emaildetails.EMAIL FROM `'+ datetime +'` INNER JOIN emaildetails ON emaildetails.TRANSPORTER_CODE = `'+ datetime +'`.TRANSPORTER_CODE WHERE `'+ datetime +'`.NO_OF_SPEED_VOILATIONS>0;';
   let query = connection.query(sql, (err,result)=>{
       if(err) throw err;
       speedviol(result);
@@ -127,7 +131,7 @@ app.get('/speedviolations', (req, res) => {
 
 //stoppage violations
 app.get('/stopviolations', (req, res) => {
-  let sql = 'SELECT emaildetails.EMAIL FROM test1 INNER JOIN emaildetails ON emaildetails.TRANSPORTER_CODE = Test1.TRANSPORTER_CODE WHERE test1.NO_OF_STOPPAGE_VOILATIONS>0;';
+  let sql = 'SELECT emaildetails.EMAIL FROM `'+ datetime +'` INNER JOIN emaildetails ON emaildetails.TRANSPORTER_CODE = `'+ datetime +'`.TRANSPORTER_CODE WHERE `'+ datetime +'`.NO_OF_STOPPAGE_VOILATIONS>0;';
   let query = connection.query(sql, (err,result)=>{
       if(err) throw err;
       stoppageviol(result);
@@ -137,7 +141,7 @@ app.get('/stopviolations', (req, res) => {
 
 //route violations
 app.get('/routeviolations', (req, res) => {
-  let sql = 'SELECT emaildetails.EMAIL FROM test1 INNER JOIN emaildetails ON emaildetails.TRANSPORTER_CODE = Test1.TRANSPORTER_CODE WHERE test1.NO_OF_ROUTE_VOILATIONS>0;';
+  let sql = 'SELECT emaildetails.EMAIL FROM `'+ datetime +'` INNER JOIN emaildetails ON emaildetails.TRANSPORTER_CODE = `'+ datetime +'`.TRANSPORTER_CODE WHERE `'+ datetime +'`.NO_OF_ROUTE_VOILATIONS>0;';
   let query = connection.query(sql, (err,result)=>{
       if(err) throw err;
       routeviol(result);
