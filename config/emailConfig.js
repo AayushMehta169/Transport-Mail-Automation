f903cae5f965ace5f3ed578d0ca98b5d1064b268
Aryan <51026686+Violent-Idiot@users.noreply.github.com>
2020-05-19 17:01:04 +0530

    email err

--- config/emailConfig.js
+++ config/emailConfig.js
@@ -1,17 +1,15 @@
 let nodemailer = require("nodemailer");
 let transporter = nodemailer.createTransport({
-    service: "gmail",
-    // host: "smtp.ethereal.email",
-    // port: 587,
-    // secure: false, // true for 465, false for other ports
-    auth: {
-      user: "",
-      pass: "",
-    },
-    tls: {
-      rejectUnauthorized: false,
-    },
-  });
+  service: "gmail",
+  // host: "smtp.ethereal.email",
+  // port: 587,
+  // secure: false, // true for 465, false for other ports
+  auth: {
+    user: "",
+    pass: "",
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
 module.exports = transporter;
-
-
--- controller/totalVoilationsMail.js
+++ controller/totalVoilationsMail.js
@@ -2,44 +2,56 @@ const connection = require("../config/config");
 const fs = require("fs");
 const transporter = require("../config/emailConfig");
 
-
 const date = new Date();
-const datetime = date.getDate()+" "+date.getMonth()+" "+date.getFullYear();
+const datetime =
+  date.getDate() + " " + date.getMonth() + " " + date.getFullYear();
 // +"-"+date.getHours()+":"+date.getMinutes()+":"+date.getSeconds()
 
 let totv = [];
 
 function totalv(value) {
-    totv = value;
-    for (i in totv) {
-      console.log(totv[i].EMAIL);
-    }
+  totv = value;
+  for (i in totv) {
+    console.log(totv[i].EMAIL);
   }
+}
 
-module.exports.totalvoilations=function(req, res) {
-    let sql ='SELECT emaildetails.EMAIL FROM `'+ datetime +'` INNER JOIN emaildetails ON emaildetails.TRANSPORTER_CODE = `'+ datetime +'`.TRANSPORTER_CODE WHERE `'+ datetime +'`.TOTAL_TRIPS_WITH_VOILATION>2;';
-    let query = connection.query(sql, (err, emails) => {
-      if (err) {
-        throw err;
-      } else {
-        totalv(emails);
-          let mailer = async (no, emails) => {
-          for (i in emails){
+module.exports.totalvoilations = function (req, res) {
+  let sql =
+    "SELECT emaildetails.EMAIL FROM `" +
+    datetime +
+    "` INNER JOIN emaildetails ON emaildetails.TRANSPORTER_CODE = `" +
+    datetime +
+    "`.TRANSPORTER_CODE WHERE `" +
+    datetime +
+    "`.TOTAL_TRIPS_WITH_VOILATION>2;";
+  let query = connection.query(sql, (err, emails) => {
+    if (err) {
+      throw err;
+    } else {
+      totalv(emails);
+      let mailer = async (no, emails) => {
+        for (i in emails) {
           let info = await transporter.sendMail({
             from: '"yoman" <example@yo.com>', // sender address
             to: emails[i].EMAIL, // list of receivers
             subject: "Hello", // Subject line
             text: "Hello world", // plain text body
             html: "<b>Hello world?</b>", // html body
-            
+            dsn: {
+              id: "123",
+              return: "headers",
+              notify: ["failure", "delay"],
+              recipient: "",
+            },
           });
           console.log(emails[i].EMAIL);
           console.log("Message sent: %s", info.messageId);
-          }
-          console.log("All Mails Sent!!");
-          res.send(emails);
-        };
-        mailer(emails.length, emails);
-      }
-    });
-  }
+        }
+        console.log("All Mails Sent!!");
+        res.send(emails);
+      };
+      mailer(emails.length, emails);
+    }
+  });
+};
