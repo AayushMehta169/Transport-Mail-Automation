04685550fc54b1d63d5934ec1882b594339ffc50
Akshat Khanna <khannakshat7@gmail.com>
2020-05-24 13:27:07 +0530

    Added Validations

--- controller/mailer.js
+++ controller/mailer.js
@@ -8,25 +8,39 @@ module.exports.mailer = async (no, emails, type) => {
   let speedviolationfile = "Speed_violation.docx";
   let stoppageviolationfile = "Stoppage_violation.docx";
 
-  let routeViolationattach = {filename:routeViolationfile,path:"./public/attachment/" + routeViolationfile,};
-  let speedViolationattach = {filename:speedviolationfile,path:"./public/attachment/" + speedviolationfile,};
-  let stoppageViolationattach = {filename:stoppageviolationfile,path:"./public/attachment/" + stoppageviolationfile,};
+  let routeViolationattach = {
+    filename: routeViolationfile,
+    path: "./public/attachment/" + routeViolationfile,
+  };
+  let speedViolationattach = {
+    filename: speedviolationfile,
+    path: "./public/attachment/" + speedviolationfile,
+  };
+  let stoppageViolationattach = {
+    filename: stoppageviolationfile,
+    path: "./public/attachment/" + stoppageviolationfile,
+  };
 
   for (i in emails) {
     let attach = [];
-    if(emails[i].NO_OF_ROUTE_VOILATIONS>0){attach.push(routeViolationattach)};
-    if(emails[i].NO_OF_SPEED_VOILATIONS>0){attach.push(speedViolationattach)};
-    if(emails[i].NO_OF_STOPPAGE_VOILATIONS>0){attach.push(stoppageViolationattach)};
-    console.log(attach);
-    if(type === 4){
+    if (emails[i].NO_OF_ROUTE_VOILATIONS > 0) {
+      attach.push(routeViolationattach)
+    };
+    if (emails[i].NO_OF_SPEED_VOILATIONS > 0) {
+      attach.push(speedViolationattach)
+    };
+    if (emails[i].NO_OF_STOPPAGE_VOILATIONS > 0) {
+      attach.push(stoppageViolationattach)
+    };
+    if (type === 4) {
       let info = await transporter.sendMail({
         from: '"yoman" <example@yo.com>', // sender address
         to: emails[i].EMAIL, // list of receivers
         subject: "Violation", // Subject line
-        html: "<b>Violation"+"<br>"+emails[i].NO_OF_STOPPAGE_VOILATIONS+"<br>"+emails[i].NO_OF_SPEED_VOILATIONS
-        +"<br>"+emails[i].NO_OF_ROUTE_VOILATIONS+"<br>"+emails[i].TANK_TRUCK_NUMBER+"</b>", // html body
+        html: "<b>Violation" + "<br>" + emails[i].NO_OF_STOPPAGE_VOILATIONS + "<br>" + emails[i].NO_OF_SPEED_VOILATIONS +
+          "<br>" + emails[i].NO_OF_ROUTE_VOILATIONS + "<br>" + emails[i].TANK_TRUCK_NUMBER + "</b>", // html body
 
-        attachments:attach,
+        attachments: attach,
         dsn: {
           id: "123",
           return: "headers",
@@ -41,19 +55,17 @@ module.exports.mailer = async (no, emails, type) => {
     // Mailing Script ends here ------------------------------
     // Below functions are for future purpose ------------------
 
-    if(type === 1){
+    if (type === 1) {
       let info = await transporter.sendMail({
         from: '"yoman" <example@yo.com>', // sender address
         to: emails[i].EMAIL, // list of receivers
         subject: "Route Violation", // Subject line
         text: "Route Violation", // plain text body
         html: "<b>Route Violation?</b>", // html body
-        attachments: [
-          {
-            filename:"Route Violation.docx",
-            path:"./public/attachments/route_violation.docx",
-          },
-        ],
+        attachments: [{
+          filename: "Route Violation.docx",
+          path: "./public/attachments/route_violation.docx",
+        }, ],
         dsn: {
           id: "123",
           return: "headers",
@@ -67,20 +79,18 @@ module.exports.mailer = async (no, emails, type) => {
       console.log(emails[i].EMAIL);
       console.log("Message sent: %s", info.messageId);
     }
-    
-    if(type === 2){
+
+    if (type === 2) {
       let info = await transporter.sendMail({
         from: '"yoman" <example@yo.com>', // sender address
         to: emails[i].EMAIL, // list of receivers
         subject: "Speed Violation", // Subject line
         text: "Speed Violation", // plain text body
         html: "<b>Speed Violation?</b>", // html body
-        attachments: [
-          {
-            filename:"Speed Violation.docx",
-            path:"./public/attachments/Speed_violation.docx",
-          },
-        ],
+        attachments: [{
+          filename: "Speed Violation.docx",
+          path: "./public/attachments/Speed_violation.docx",
+        }, ],
         dsn: {
           id: "123",
           return: "headers",
@@ -92,19 +102,17 @@ module.exports.mailer = async (no, emails, type) => {
       console.log("Message sent: %s", info.messageId);
     }
 
-    if(type === 3){
+    if (type === 3) {
       let info = await transporter.sendMail({
         from: '"yoman" <example@yo.com>', // sender address
         to: emails[i].EMAIL, // list of receivers
         subject: "Stoppage Violation", // Subject line
         text: "Stoppage Violation", // plain text body
         html: "<b>Stoppage Violation?</b>", // html body
-        attachments: [
-          {
-            filename:"Stoppage Violation.docx",
-            path:"./public/attachments/Stoppage_violation.docx",
-          },
-        ],
+        attachments: [{
+          filename: "Stoppage Violation.docx",
+          path: "./public/attachments/Stoppage_violation.docx",
+        }, ],
         dsn: {
           id: "123",
           return: "headers",
@@ -115,8 +123,8 @@ module.exports.mailer = async (no, emails, type) => {
       console.log(emails[i].EMAIL);
       console.log("Message sent: %s", info.messageId);
     }
- 
+
   }
   console.log("All Mails Sent!!");
-  
+
 };
