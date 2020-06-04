7e7cf132e3f3d25dba1259b5ab24d63a6575b432
AayushMehta169 <52970754+AayushMehta169@users.noreply.github.com>
2020-06-04 22:22:04 +0530

    previous uploads error handling

--- controller/viewPrevious.js
+++ controller/viewPrevious.js
@@ -1,20 +1,22 @@
 const connection = require("../config/config");
-const date = new Date();
-const datetime =
-  date.getDate() + " " + date.getMonth() + " " + date.getFullYear();
+ 
+
 
   module.exports.previousTable = function (req, res) {
+  var fs = require('fs');
+var array = fs.readFileSync('logging.txt').toString().split("\n");
+var datetime = array[array.length - 2];
     let sql =
       "SELECT * FROM `" +
       datetime +
       "`;";
     let query = connection.query(sql, (err, emails) => {
-      if (err)    throw err;
-    
+      if (err)   
+      res.render('error.ejs');
       
       else{
         
-            res.render('previous.ejs', { details: emails });  
+            res.render('previous.ejs', { details: emails, date: datetime });  
       }
       
     });
